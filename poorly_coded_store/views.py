0071702f6ab989a89391047695c4bde62608dabd
Aitassova Mereilim <mereilim@Aitassovas-MacBook-Pro.local>
2020-10-13 09:33:50 -0400

    Separate Amadon

--- poorly_coded_store/views.py
+++ poorly_coded_store/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Order, Product
+from django.db.models import Sum
 
 def index(request):
     context = {
@@ -8,9 +9,20 @@ def index(request):
     return render(request, "store/index.html", context)
 
 def checkout(request):
+
+    context = {
+        'order': Order.objects.last(),
+        'total_price_for_all_orders': Order.objects.aggregate(Sum('total_price')),
+        'total_quantity_for_all_orders':Order.objects.aggregate(Sum('quantity_ordered'))
+    }
+
+    return render(request, "store/checkout.html", context)
+
+def calculations(request):
+    product = Product.objects.get(id=request.POST['product_id'])
+    price_of_product = product.price
     quantity_from_form = int(request.POST["quantity"])
-    price_from_form = float(request.POST["price"])
-    total_charge = quantity_from_form * price_from_form
+    total_charge = quantity_from_form * price_of_product
     print("Charging credit card...")
     Order.objects.create(quantity_ordered=quantity_from_form, total_price=total_charge)
-    return render(request, "store/checkout.html")
+    return redirect('/checkout')
